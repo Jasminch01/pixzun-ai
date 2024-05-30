73c5589e5d22c3166bfc99a75f8151e6abe3437e
jasmin <chjasmin27@gmail.com>
2024-05-30 18:51:55 +0600

    hero section, appbar, steps section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/Container";
 import Hero from "@/components/Hero";
+import Steps from "@/components/Steps";
 
 export default function Home() {
   return (
     <main className="min-h-screen mt-32">
       <Container>
         <Hero />
+        <Steps/>
       </Container>
     </main>
   );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,29 +4,29 @@ const Hero = () => {
   return (
     <div>
       <div className="text-center text-white px-5 md:px-0">
-        <p className="md:text-5xl text-3xl font-bold">
-          Make Standout Content <br></br> With Stunning{" "}
+        <p className="md:text-[52px] md:leading-extra-loose text-3xl font-bold">
+          Make Standout Content <br/> With Stunning{" "}
           <span className="gradient-text">AI</span>
         </p>
-        <p className="mt-6 hidden md:block">
-          Elevate your brand with our AI-powered product image solutions. <br />{" "}
+        <p className="mt-7 hidden md:block md:text-lg text-sm text-gray-400">
+          Elevate your brand with our AI-powered product image solutions. <br />
           giving you professional, high-quality visuals effortlessly.
         </p>
-        <p className="mt-6 w-[300px] mx-auto text-sm md:hidden">
+        <p className="mt-6 w-[300px] mx-auto text-sm md:hidden text-gray-400">
           Elevate your brand with our AI-powered product image solutions. giving
           you professional, high-quality visuals effortlessly.
         </p>
-        <div className="mt-6">
-          <button className="bg-button-gradient md:py-3 md:px-4 p-2 text-sm md:text-base rounded-full text-white">
-            Get Started
+        <div className="mt-11">
+          <button className="bg-button-gradient md:h-[68px] md:w-[197px] md:px-0 md:py-0 px-3 py-2 text-sm md:text-base rounded-full text-white">
+            Get started
           </button>
         </div>
-        <div className=" relative flex items-center justify-center mt-20">
-          <div className="absolute bg-bg-lighter blur-3xl md:w-[600px] md:h-[400px] w-[400px] h-[200px] rounded -z-10"></div>
+        <div className=" relative flex items-center justify-center mt-[5.56rem]">
+          <div className="absolute bg-bg-lighter blur-3xl md:w-[700px] md:h-[521px] w-[300px] h-[200px] rounded -z-10"></div>
           <div className=" p-2 bg-border-gradient rounded">
             <iframe
-              className="md:w-[600px] md:h-[400px] w-[300px] h-[200px]"
-              src="https://www.youtube.com/embed/TU1gMloI0kc"
+              className="lg:w-[894px] lg:h-[521px] md:w-[600px] md:h-[400px] w-[300px] h-[200px]"
+              src="https://www.youtube.com/embed/TU1gMloI0kc?autoplay=1&mute=1&loop=1&playlist=TU1gMloI0kc"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
