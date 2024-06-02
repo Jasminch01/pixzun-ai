e221c10cc5b8d1bf77eb18a3989270cfe88db8ac
jasmin <chjasmin27@gmail.com>
2024-06-03 00:37:38 +0600

    fixed transparent bg appbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Appbar from "@/components/Appbar";
+import Container from "@/components/Container";
 
 const poppins = Poppins({
   variable: "--font-poppins",
