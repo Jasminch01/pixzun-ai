2babad13494c44046d65bf3f270b67e6258cdc23
jasmin <chjasmin27@gmail.com>
2024-12-02 22:59:27 +0600

    add tamplate propmt, images, hot fix

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  // baseURL: "http://localhost:5000",
-  baseURL: "https://pixzunserver-pbg7n4s6.b4a.run",
+  baseURL: "http://localhost:5000",
+  // baseURL: "https://pixzunserver-pbg7n4s6.b4a.run",
   // baseURL: "https://pixzun-ai-server.onrender.com",
   withCredentials: true,
 });
